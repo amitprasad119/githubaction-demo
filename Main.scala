e77df3385909e34ba8aed1d06121738e7f5a1fcc
Amit <34961862+amitprasad119@users.noreply.github.com>
2023-03-10 15:07:01 +0530

    updated main

--- Main.scala
+++ Main.scala
@@ -1,7 +1,6 @@
 object Main {
-    
-    def main(args: Array[String]) = {                 
-       println("Hello, world")
-    }
+  def main(args: Array[String]) = {
+    println("Hello, world")
+  }
 
 }
